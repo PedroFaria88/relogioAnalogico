76c373a82424b9eee3f3599cc32cf22f82576747
Pedro Faria <114203499+PedroFaria88@users.noreply.github.com>
2023-03-29 12:23:11 -0300

    animação

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-setInterval(setClock, 1000)
+setInterval(moveClock, 1000)
+
+const pontHora = document.querySelector("[data-pont-hora]")
+const pontMin = document.querySelector("[data-pont-min]")
+const pontSeg = document.querySelector("[data-pont-seg]")
 
 function setClock(){
 
     const currentDate = new Date();
-
-
-
+    const secondsRatio = currentDate.getSeconds() / 60 ;
+    const minutesRatio = currentDate.getMinutes() / 60 ;
+    const hourRatio = (minutesRatio + currentDate.getHours()) / 12;
+    setRotation(pontHora, hourRatio);
+    setRotation(pontMin, minutesRatio);
+    setRotation(pontSeg, secondsRatio);
 }
+
+function setRotation(element, ratio){
+
+    element.style.setProperty("--rotation", ratio * 360 );
+}
+
+function moveClock(){
+    
+}
+
+function setMove(element){
+    
+    element.style.setProperty("--rotation", ratio * 360 );
+}
+
+setClock();
